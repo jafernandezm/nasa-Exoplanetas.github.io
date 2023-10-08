5a7474f9a63c38d4680722e7f69a54c0f1d4ee8b
Jhon Fernandez <fernandez.jhon.alan@gmail.com>
2023-10-08 06:00:49 -0400

    ejemplo2

--- js/codigo.js
+++ js/codigo.js
@@ -28,3 +28,30 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   });
 });
+
+
+
+function cargarImagenesEnCarrusel(numero, carpeta, elementoId) {
+  const carrusel = document.getElementById(elementoId);
+  const imagenesCarrusel = carrusel.querySelector('.imagenes-carrusel');
+  const botonSiguiente = carrusel.querySelector('.siguiente');
+
+  // Elimina todas las imágenes existentes en el carrusel
+  imagenesCarrusel.innerHTML = '';
+
+  // Carga las imágenes en el carrusel
+  for (let i = 1; i <= numero; i++) {
+    const imagen = document.createElement('img');
+    imagen.src = `${carpeta}/${i} Flora.png`;
+    imagen.alt = `Imagen ${i}`;
+    imagenesCarrusel.appendChild(imagen);
+  }
+
+  
+}
+
+// Llama a la función para cargar imágenes en el carrusel
+const numeroDeImagenes = 5; // Cambia esto al número deseado de imágenes
+const carpetaDeImagenes = 'img/flora'; // Cambia esto a la ruta de tu carpeta
+const elementoCarruselId = 'imagenes-carrusel'; // Cambia esto al ID de tu elemento HTML
+cargarImagenesEnCarrusel(numeroDeImagenes, carpetaDeImagenes, elementoCarruselId);
